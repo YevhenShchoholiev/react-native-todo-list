6eecc67afe977108a02e238b1f10ed73232be357
YevhenShchoholiev <yevhen.lev.dp@gmail.com>
2020-06-17 17:40:11 +0300

    add AddTodo in progress

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar';
+import { AddTodo } from './src/AddTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }])
+  }
+
   return (
-    <View style={styles.container}>
-      <Navbar title="Todo App"/>
+    <View >
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo}/>
+
+        <View>
+          {todos.map((todo,index)=>{
+            return <Text key={index}>{todo.title}</Text>
+          })}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    paddingHorizontal: 30,
+    paddingVertical: 20
   },
 });
