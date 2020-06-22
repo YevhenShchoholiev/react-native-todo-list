cd9de0fe9d08688f4161a310a9b66f99d6b3969c
YevhenShchoholiev <yevhen.lev.dp@gmail.com>
2020-06-22 15:27:12 +0300

    EditModal in progress

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import { THEME } from '../theme'
 import { AppCard } from '../components/ui/AppCard'
+import { EditModal } from '../components/EditModal'
 
 export const TodoScreen = ({ goBack, todo, onRemove }) => {
+    const [modal, setModal] = useState(false)
     return (
         <View>
+            <EditModal visible={modal} onCancel={() => setModal(false)} />
             <AppCard style={styles.card}>
                 <Text style={styles.title}>{todo.title}</Text>
-                <Button title='Edit' />
+                <Button title='Edit' onPress={() => setModal(true)} />
             </AppCard>
             <View style={styles.buttons}>
                 <View style={styles.button}>
@@ -16,7 +19,7 @@ export const TodoScreen = ({ goBack, todo, onRemove }) => {
                 </View>
 
                 <View style={styles.button}>
-                    <Button title="Delete" color={THEME.DANGER_COLOR} onPress={() => onRemove(todo.id )} />
+                    <Button title="Delete" color={THEME.DANGER_COLOR} onPress={() => onRemove(todo.id)} />
                 </View>
             </View>
 
